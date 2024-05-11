9d0739464820cc7654d23b8b4a9945fa729fa75c
Пользователь <@DESKTOP-QRJDCVI>
2024-05-11 22:38:47 +0300

    Добавьте файлы проекта.

---
+++ пз2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int number;
+    do {
+        cout << "Please enter a number > 60 or press Enter to exit: ";
+        string input;
+        getline(cin, input);
+        if (input.empty()) {
+            cout << "Exiting..." << endl;
+            break;
+        }
+        number = stoi(input);
+        if (number <= 60) {
+            cout << "The entered number is not > 60. Please try again." << endl;
+        }
+    } while (number <= 60);
+    return 0;
+}
+
---
+++ пз2/пз2.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <vector>
+#include <Windows.h>
+
+// Базовий клас
+class Function {
+public:
+    virtual double compute(double x) = 0; // Віртуальна функція
+};
+
+// Похідний клас 1
+class LinearFunction : public Function {
+public:
+    double compute(double x) override {
+        return x; // f(x) = x
+    }
+};
+
+// Похідний клас 2
+class QuadraticFunction : public Function {
+public:
+    double compute(double x) override {
+        return x * x; // f(x) = x^2
+    }
+};
+
+// Похідний клас 3
+class CubicFunction : public Function {
+public:
+    double compute(double x) override {
+        return x * x * x; // f(x) = x^3
+    }
+};
+
+// Клас-контейнер
+class FunctionContainer {
+private:
+    std::vector<Function*> functions;
+public:
+    void add(Function* function) {
+        functions.push_back(function);
+    }
+    double computeSum(double x) {
+        double sum = 0;
+        for (Function* function : functions) {
+            sum += function->compute(x);
+        }
+        return sum;
+    }
+};
+
+int main() {
+    SetConsoleCP(1251); SetConsoleOutputCP(1251);
+    FunctionContainer container;
+    container.add(new LinearFunction());
+    container.add(new QuadraticFunction());
+    container.add(new CubicFunction());
+
+    double x;
+    std::cout << "Введіть значення для X: ";
+    std::cin >> x;
+
+    std::cout << "Сума значень функцій для x=" << x << " є " << container.computeSum(x) << std::endl;
+
+    return 0;
+}
